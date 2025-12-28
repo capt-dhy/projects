9bbda7d4fded6caf6fe09815e5b07e9a56683500
Dawood yusuf Hassan <dawoodhassan0555@outlook.com>
2025-12-28 12:36:33 +0100

    Comment out middleware array in app.php
    Comment out middleware definition in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        protected $middleware = [
+       /*  protected $middleware = [
             Cors::class
-        ];
+        ]; */
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
